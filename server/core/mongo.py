201682968cb795bc0811bb0ecfc87e61cff5316a
anmol52490 <anmol52490@gmail.com>
2025-11-19 19:44:10 +0530

    removed core from gitignore

---
+++ server/core/mongo.py
@@ -0,0 +1,28 @@
+from pymongo.mongo_client import MongoClient
+import os
+import certifi
+from dotenv import load_dotenv
+load_dotenv()
+
+uri = os.getenv("MONGO_URI")
+
+client = MongoClient(uri,tlsCAFile=certifi.where(),
+    serverSelectionTimeoutMS=5000 )
+
+try:
+    client.admin.command('ping')
+    print("✅ Successfully connected to MongoDB!")
+except Exception as e:
+    print("❌ MongoDB connection error:", e)
+
+db = client["interview_ai"]
+# This is now the only collection you need for interview data
+sessions_col = db["interview_sessions"]
+qa_col = db["qa_history"]
+persona_col = db["persona_reports"]
+lesson_plan = db["lesson_plans"]
+user = db["users"]
+assignment = db["assignments"]
+content_col = db["generated_content"]
+assignments_col = db["assignments"]
+generated_content_col = db["generated_content"]
